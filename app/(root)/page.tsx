5326c6cc91e896abfb7f175d136e220485b1a4a6
Jorge-Gil <jorge.gil@uao.edu.co>
2024-05-05 16:58:58 -0500

    Implement doughnut chart and animated counter for the home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,31 @@
+import Headerbox from '@/components/Headerbox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+
+  const loggedIn = {
+    firstName: 'Jorge',
+  }
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <Headerbox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext ="Access and Manage your Account and Transactions efficiently"
+          />
+          <TotalBalanceBox 
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={69000.35}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
