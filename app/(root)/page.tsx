c30bd0d36e33f8e5e29eefeb25314bfb18fbc376
Jorge-Gil <jorge.gil@uao.edu.co>
2024-05-13 14:45:45 -0500

    add force-dynamic

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -8,17 +8,18 @@ import React from "react";
 
 const Home = async ({searchParams: {id, page}}:SearchParamProps) => {
 
-  const currentPage = Number(page as string) || 1
-  const loggedIn = await getLoggedInUser()
-  const accounts = await getAccounts({userId: loggedIn.$id})
+  const currentPage = Number(page as string) || 1;
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ 
+    userId: loggedIn.$id 
+  })
 
-  if (!accounts) return;
-
-  const accountsData = accounts?.data
-
+  if(!accounts) return;
+  
+  const accountsData = accounts?.data;
   const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
 
-  const account = await getAccount({ appwriteItemId });
+  const account = await getAccount({ appwriteItemId })
 
 
   // console.log({
